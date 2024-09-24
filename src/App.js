4afc36459ac3e70895ab5257f707bf9dd4f325d9
Sol Cappella <solcappella@Sols-MacBook-Pro-2.local>
2024-09-24 18:20:39 -0300

    Arreglo header y footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
+import Footer from './components/Layout/Footer/Footer';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
